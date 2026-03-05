54d2d42839030cbb3809d2dbcd9c8a63eddf689f
kolheabhinandan-pixel <kolhe.abhinandan@hrjohnsonindia.com>
2026-03-05 16:35:59 +0530

    app.py

--- app.py
+++ app.py
@@ -142,6 +142,6 @@ def api_trends(date_str):
     trends = load_json_file(os.path.join(base_path, "trends.json"))
     return jsonify(trends)
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host="0.0.0.0", port=port)
